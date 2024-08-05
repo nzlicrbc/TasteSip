3e41d69949c93e4666485ef4ca0211a80189d6e6
nzlicrbc <nzlcn10@hotmail.com>
2024-08-06 00:07:15 +0300

    add category and categoryResponse data model

---
+++ app/src/main/java/com/example/tastesip/data/model/Category.kt
@@ -0,0 +1,5 @@
+package com.example.tastesip.data.model
+
+data class Category(
+    val strCategory: String
+)
---
+++ app/src/main/java/com/example/tastesip/data/model/Categoryresponse.kt
@@ -0,0 +1,5 @@
+package com.example.tastesip.data.model
+
+data class Categoryresponse(
+    val categories: List<Category>
+)
