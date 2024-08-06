dee9568bf7b04ca72d0b6ea5ee8e7794f1b21bcb
nzlicrbc <nzlcn10@hotmail.com>
2024-08-06 23:50:30 +0300

    create CategoryViewModel

---
+++ app/src/main/java/com/example/tastesip/ui/viewmodel/CategoryViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.tastesip.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tastesip.data.model.Category
+import com.example.tastesip.data.repository.RecipeRepository
+import com.example.tastesip.util.Resource
+import kotlinx.coroutines.launch
+
+class CategoryViewModel(private val repository: RecipeRepository) : ViewModel() {
+
+    private val _mealCategories = MutableLiveData<Resource<List<Category>>>()
+    val mealCategories: LiveData<Resource<List<Category>>> = _mealCategories
+
+    fun fetchMealCategories() {
+        viewModelScope.launch {
+            _mealCategories.value = Resource.Loading()
+            _mealCategories.value = repository.getMealCategories()
+        }
+    }
+}
