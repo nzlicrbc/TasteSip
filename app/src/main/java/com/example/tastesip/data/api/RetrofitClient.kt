97a41025df701ce5839f361a9c12d98ff488dec4
nzlicrbc <nzlcn10@hotmail.com>
2024-08-08 23:46:02 +0300

    create constants file

--- app/src/main/java/com/example/tastesip/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/tastesip/data/api/RetrofitClient.kt
@@ -1,11 +1,11 @@
 package com.example.tastesip.data.api
 
+import com.example.tastesip.util.Constants.COCKTAIL_BASE_URL
+import com.example.tastesip.util.Constants.MEAL_BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val MEAL_BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
-    private const val COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
 
     private val mealRetrofit by lazy {
         Retrofit.Builder()
@@ -21,11 +21,11 @@ object RetrofitClient {
             .build()
     }
 
-    val mealApiService: ApiService by lazy {
-        mealRetrofit.create(ApiService::class.java)
+    val mealApiService: MealApiService by lazy {
+        mealRetrofit.create(MealApiService::class.java)
     }
 
-    val cocktailApiService: ApiService by lazy {
-        cocktailRetrofit.create(ApiService::class.java)
+    val cocktailApiService: CocktailApiService by lazy {
+        cocktailRetrofit.create(CocktailApiService::class.java)
     }
 }
---
+++ app/src/main/java/com/example/tastesip/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.tastesip.util
+
+object Constants {
+    const val MEAL_BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+    const val COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
+}
