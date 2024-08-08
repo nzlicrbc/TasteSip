aac7a968d3f6ccd8c5ea976a2de43ddabf56a534
nzlicrbc <nzlcn10@hotmail.com>
2024-08-08 23:46:35 +0300

    delete category model files

--- app/src/main/java/com/example/tastesip/data/model/Category.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tastesip.data.model
-
-data class Category(
-    val strCategory: String
-)
--- app/src/main/java/com/example/tastesip/data/model/Categoryresponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tastesip.data.model
-
-data class Categoryresponse(
-    val categories: List<Category>
-)
