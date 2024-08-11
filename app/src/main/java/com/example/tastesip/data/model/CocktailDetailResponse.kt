386057dec20422b10a1b0d98ae3b21fd5c628ef4
nzlicrbc <nzlcn10@hotmail.com>
2024-08-11 21:09:41 +0300

    update detail response files

--- app/src/main/java/com/example/tastesip/data/model/CocktailDetailResponse.kt
+++ app/src/main/java/com/example/tastesip/data/model/CocktailDetailResponse.kt
@@ -53,4 +53,28 @@ data class CocktailDetail(
     val strMeasure18: String?,
     val strMeasure19: String?,
     val strMeasure20: String?
-)
+) {
+    val ingredientsAndMeasuresList
+        get() = mutableListOf(
+            strIngredient1 to strMeasure1,
+            strIngredient2 to strMeasure2,
+            strIngredient3 to strMeasure3,
+            strIngredient4 to strMeasure4,
+            strIngredient5 to strMeasure5,
+            strIngredient6 to strMeasure6,
+            strIngredient7 to strMeasure7,
+            strIngredient8 to strMeasure8,
+            strIngredient9 to strMeasure9,
+            strIngredient10 to strMeasure10,
+            strIngredient11 to strMeasure11,
+            strIngredient12 to strMeasure12,
+            strIngredient13 to strMeasure13,
+            strIngredient14 to strMeasure14,
+            strIngredient15 to strMeasure15,
+            strIngredient16 to strMeasure16,
+            strIngredient17 to strMeasure17,
+            strIngredient18 to strMeasure18,
+            strIngredient19 to strMeasure19,
+            strIngredient20 to strMeasure20
+        ).filter { it.first != null || it.second != null }.toMutableList()
+}
